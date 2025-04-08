1d0f16fa95305652686a8f14808d82254ca45bac
mralfiem591 <iamamccabe@gmail.com>
2025-04-08 11:28:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='quillpy',
-    version='0.1.4',
+    version='0.1.5',
     description='A lightweight terminal-based text editor',
     long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
