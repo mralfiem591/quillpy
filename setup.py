13da3c6e4df7b936d349c44aad0c94fea07b607b
mralfiem591 <iamamccabe@gmail.com>
2025-04-08 09:38:34 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+setup(
+    name='quillpy',
+    version='0.1.0',
+    description='A lightweight terminal-based text editor',
+    author='Alfie',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/quillpy',
+    license='MIT',
+    python_requires='>=3.6',
+    project_urls={
+        'Bug Tracker': 'https://github.com/yourusername/quillpy/issues',
+        'Source Code': 'https://github.com/yourusername/quillpy',
+    },
+    packages=['quillpy'],
+    install_requires=[
+        'windows-curses; platform_system=="Windows"',
+        'pywin32; platform_system=="Windows"'
+    ],
+    entry_points={
+        'console_scripts': [
+            'quillpy=quillpy.quill:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Editors :: Documentation',
+        'Topic :: Utilities',
+        'Operating System :: OS Independent',
+    ]
+)
